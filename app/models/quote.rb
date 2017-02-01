4a01f11dd43a48e5ddffb383c4732ae85879bd0a
Drot88 <Daniel.Rotenberg88@gmail.com>
2017-01-31 22:27:05 -0500

    Added validation for saying and movie

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+  validates :saying, presence: true, length: { maximum: 150, minimum: 3 }
+  validates :movie, presence: true, length: { maximum: 75, minimum: 3 }
 end
